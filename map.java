b697a4c3517f28db28b4a04f68ecddf3d59ea9d9
DELIN-Maxime <98822652+DELIN-Maxime@users.noreply.github.com>
2023-02-28 01:26:44 +0100

    Update map.java

--- map.java
+++ map.java
@@ -1,48 +1,156 @@
 package rpg;
 
-public class map {
-	public static int[][] map1 = new int[15][15];
-	
-	public map(){
-		for(int i=0;i<map1.length;++i) {
-			for(int j=0;j<map1.length;++j) {
-				map1[i][j]=0;
+public class Map {
+
+	private int nbRow;
+	private int nbCol;
+	private char[][] map1;
+
+
+	public Map() {
+		nbRow = 15;
+		nbCol = 15;
+		map1 = new char[nbRow][nbCol];
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				map1[i][j]='.';
 			}
 		}
-		map1[14][0]=2;
-		map1[12][0]=3;
-		map1[13][0]=5;
-		map1[14][2]=1;
 	}
 
-	
-	public static void showMap() {
-		for(int i=0;i<map1.length;++i) {
-			System.out.print('|');
-			for(int j=0;j<map1.length-1;++j) {
-				System.out.print("\t");
-				if(map1[i][j]==0) { //case vide
-					System.out.print('-');
-				}
-				else if(map1[i][j]==1) { //obstacle
-					System.out.print('#');
-				}
-				else if(map1[i][j]==2) { //héros
-					System.out.print('+');
-				}
-				else if(map1[i][j]==3) { //ennemi
-					System.out.print('x');
-				}
-				else if(map1[i][j]==4) { //boss
-					System.out.print('X');
-				}
-				else if(map1[i][j]==5) { //item
-					System.out.print('°');
-				}
+
+	public char getCell(int row, int col) {
+		return map1[row][col];
+	}
+
+
+	public void showMap() {
+		System.out.println();
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				System.out.print(" | " + map1[i][j]);
 			}
-			System.out.print("\t");
-			System.out.println('|');
+			System.out.println(" | ");
 		}
 		System.out.println();
+	}
+
+
+	public void place(int row, int col, char type) {
+		if(row<0||row>nbRow||col<0||col>nbCol) {
+			System.out.println("Placement error (outside the borders)");
+			return;
+		}
+		if(map1[row][col]=='.') {
+			map1[row][col]=type;
+		}
+		else
+			System.out.println("Placement error (cell is not empty)");
+	}
+
+
+	public void moveUpward() {
+		char tmp;
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				if(map1[i][j]=='O') {
+					if(i-1>=0 && map1[i-1][j]=='.') { //if empty cell
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i-1][j]=tmp;
+					}
+					if(i+1>=0 && map1[i-1][j]=='x' || map1[i-1][j]=='X') { //if enemy x=enemy, X=boss
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i-1][j]=tmp;
+					}
+					if(i+1>=0 && map1[i-1][j]=='K') { //if item K=item
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i-1][j]=tmp;
+					}
+				}
+			}
+		}
+	}
+
+
+
+	public void moveDownward() {
+		char tmp;
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				if(map1[i][j]=='O') {
+					if(i+1<nbRow && map1[i+1][j]=='.') { //if empty cell
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i+1][j]=tmp;
+					}
+					if(i+1<nbRow && map1[i+1][j]=='x' || map1[i+1][j]=='X') { //if enemy x=enemy, X=boss
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i+1][j]=tmp;
+					}
+					if(i+1<nbRow&& map1[i+1][j]=='K') { //if item K=item
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i+1][j]=tmp;
+					}
+				}
+			}
+		}
+	}
+
+
+
+	public void moveLeftward() {
+		char tmp;
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				if(map1[i][j]=='O') {
+					if(i-1>=0 && map1[i][j-1]=='.') { //if empty cell
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i-1][j]=tmp;
+					}
+					if(i+1>=0 && map1[i][j-1]=='x' || map1[i][j-1]=='X') { //if enemy x=enemy, X=boss
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i][j-1]=tmp;
+					}
+					if(i+1>=0 && map1[i][j-1]=='K') { //if item K=item
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i][j-1]=tmp;
+					}
+				}
+			}
+		}
+	}
+
+
+	public void moveRightward() {
+		char tmp;
+		for(int i=0;i<nbRow;++i) {
+			for(int j=0;j<nbCol;++j) {
+				if(map1[i][j]=='O') {
+					if(i-1>=0 && map1[i][j+1]=='.') { //if empty cell
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i][j+1]=tmp;
+					}
+					if(i+1<nbRow && map1[i][j+1]=='x' || map1[i][j+1]=='X') { //if enemy x=enemy, X=boss
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i][j+1]=tmp;
+					}
+					if(i+1<nbRow && map1[i][j+1]=='K') { //if item K=item
+						tmp=map1[i][j];
+						map1[i][j]='.';
+						map1[i][j+1]=tmp;
+					}
+				}
+			}
+		}
 	}
 }
